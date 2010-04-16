189dc89f79d8abc3762a34ce66fd97339c92283b
Renaudeau Gaetan <pro@grenlibre.fr>
2010-04-16 09:01:55 +0200

    Begin a Config importer/exporter

--- javascripts/main.js
+++ javascripts/main.js
@@ -127,4 +127,192 @@
   }();
   $(document).ready(Sounds.init);
   
+  /**
+    *  Base64 encode / decode
+    *  http://www.webtoolkit.info/
+    **/
+  var Base64 = {
+    // private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    // public method for encoding
+    encode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+      var i = 0;
+      input = Base64._utf8_encode(input);
+      while (i < input.length) {
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+        if (isNaN(chr2)) {
+          enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+          enc4 = 64;
+        }
+        output = output +
+        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+      }
+      return output;
+    },
+
+    // public method for decoding
+    decode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3;
+      var enc1, enc2, enc3, enc4;
+      var i = 0;
+      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+      while (i < input.length) {
+        enc1 = this._keyStr.indexOf(input.charAt(i++));
+        enc2 = this._keyStr.indexOf(input.charAt(i++));
+        enc3 = this._keyStr.indexOf(input.charAt(i++));
+        enc4 = this._keyStr.indexOf(input.charAt(i++));
+        chr1 = (enc1 << 2) | (enc2 >> 4);
+        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+        chr3 = ((enc3 & 3) << 6) | enc4;
+        output = output + String.fromCharCode(chr1);
+        if (enc3 != 64) {
+          output = output + String.fromCharCode(chr2);
+        }
+        if (enc4 != 64) {
+          output = output + String.fromCharCode(chr3);
+        }
+      }
+      output = Base64._utf8_decode(output);
+      return output;
+    },
+
+    // private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+      string = string.replace(/\r\n/g,"\n");
+      var utftext = "";
+      for (var n = 0; n < string.length; n++) {
+        var c = string.charCodeAt(n);
+        if (c < 128) {
+          utftext += String.fromCharCode(c);
+        }
+        else if((c > 127) && (c < 2048)) {
+          utftext += String.fromCharCode((c >> 6) | 192);
+          utftext += String.fromCharCode((c & 63) | 128);
+        }
+        else {
+          utftext += String.fromCharCode((c >> 12) | 224);
+          utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+          utftext += String.fromCharCode((c & 63) | 128);
+        }
+      }
+      return utftext;
+    },
+
+    // private method for UTF-8 decoding
+    _utf8_decode : function (utftext) {
+      var string = "";
+      var i = 0;
+      var c = c1 = c2 = 0;
+      while ( i < utftext.length ) {
+        c = utftext.charCodeAt(i);
+        if (c < 128) {
+          string += String.fromCharCode(c);
+          i++;
+        }
+        else if((c > 191) && (c < 224)) {
+          c2 = utftext.charCodeAt(i+1);
+          string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+          i += 2;
+        }
+        else {
+          c2 = utftext.charCodeAt(i+1);
+          c3 = utftext.charCodeAt(i+2);
+          string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+          i += 3;
+        }
+      }
+      return string;
+    }
+  };
+  
+  var Config = CONFIG = function() {
+    /* FORMAT : separate by \n and :
+      <nb of cut>
+      <bpm>
+      # for each line :
+      <letter num>:<volume (0.0 to 1.0)>:<line data>
+      
+      line data represente the checked input.
+      For the moment we will represent it by 0 and 1 line.
+      ex : 00100100100100 ..
+      
+     */
+    
+    var setConfig = function(conf) {
+      /*
+      conf.nbCut
+      conf.bpm
+      conf.tracks
+      conf.tracks[i].{letter,volume,data}
+      */
+      console.log(conf);
+    };
+    var getConfig = function() {
+      return {
+        nbCut: 16,
+        bpm: 80,
+        tracks: [
+          {letter:65, volume: 0.5, data: "0010010010010010"},
+          {letter:66, volume: 0.75, data: "0010010010010010"},
+          {letter:67, volume: 1.0, data: "0010010010010010"}
+        ]
+      };
+    };
+    
+    /**
+     * return a base64 str
+     */
+    var export = function() {
+      var conf = getConfig();
+      var str = conf.nbCut+"\n"+conf.bpm;
+      for(var l in conf.tracks) {
+        var track = conf.tracks[l];
+        str+=("\n"+track.letter+":"+track.volume+":"+track.data);
+      }
+      return Base64.encode(str);
+    };
+    var import = function(str_base64) {
+      var conf = {tracks: []};
+      var str = Base64.decode(str_base64);
+      var spl = str.split('\n');
+      if(spl.length<3)
+        return -1;
+      var nbCut = conf.nbCut = parseInt(spl[0]);
+      var bpm = conf.bpm = parseInt(spl[1]);
+      if(!nbCut || !bpm || bpm<0 || nbCut<0)
+        return -2;
+      for(var l=2; l<spl.length; ++l) {
+        var confTrack = {};
+        var line = spl[l];
+        var lineSpl = line.split(':');
+        if(lineSpl.length!=3)
+          return -3;
+        var letter = confTrack.letter = parseInt(lineSpl[0]);
+        var volume = confTrack.volume = parseFloat(lineSpl[1]);
+        confTrack.data = lineSpl[2];
+        if(!letter || letter<0 || isNaN(volume) || volume<0.0 || volume>1.0)
+          return -4;
+        conf.tracks.push(confTrack);
+      }
+      setConfig(conf);
+      return 0;
+    };
+    return {
+      export: export,
+      import: import
+    }
+  }();
+  
 }());
